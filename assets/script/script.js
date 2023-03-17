247267d80f741efcd7aae18d164b32a2c9403c61
RafaelCandeias00 <rafaelcandeias39@gmail.com>
2023-03-17 16:29:31 -0300

    @RafaelCandeias00 [Task: criando posições randômicas]

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,11 +1,32 @@
 var altura = 0
 var largura = 0
 
+// Ajustando tamanho do paclo
 function ajustaTamanhoPalcoJogo(){
     altura = window.innerHeight
     largura = window.innerWidth
-    console.log(altura, largura)
+    //console.log(largura, altura)
 }
 ajustaTamanhoPalcoJogo()
 
 
+function posicaoRandomica(){
+    // Valores aleatórios
+var posicaoX = Math.floor(Math.random() * largura) - 90
+var posicaoY = Math.floor(Math.random() * altura) - 90
+
+posicaoX = posicaoX < 0 ? 0 : posicaoX
+posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+console.log(posicaoX, posicaoY)
+
+// Criar elemento HTML e recupera caminho da imagem
+var mosquito = document.createElement("img")
+mosquito.src = "assets/img/mosca.png"
+mosquito.className = "mosquito1"
+mosquito.style.left = posicaoX + 'px'
+mosquito.style.top = posicaoY + 'px'
+mosquito.style.position = 'absolute'
+// adiciona elemento criado ao body
+document.body.appendChild(mosquito)
+}
